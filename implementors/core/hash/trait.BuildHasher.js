dab87ed53eab5a63d1f65ede61c3c0b76e1feae7
FlashCat <FlashCat@users.noreply.github.com>
2016-03-16 03:54:03 +0000

    rebuild pages at 41846a9

---
+++ implementors/core/hash/trait.BuildHasher.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['hashmap2'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.BuildHasher.html' title='core::hash::BuildHasher'>BuildHasher</a> for <a class='struct' href='hashmap2/struct.RandomState.html' title='hashmap2::RandomState'>RandomState</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
